009518a58ffc40386522d9a64b505ac49b60dbbe
zackkb <98679473+zackkb@users.noreply.github.com>
2022-03-24 21:34:37 -0500

    Express/Morgan, GET routes, port 8080
    Moved documentation.html to "public" folder;
    
    Added details to .gitignore;
    
    Added morgan framework to package.json;
    
    Imported express and morgan frameworks to index.js along with GET requests, error handling

---
+++ index.js
@@ -0,0 +1,39 @@
+// Import framework
+const express = require('express'),
+    morgan = require('morgan');
+
+const app = express();
+
+// Morgan middleware library to log all requests
+app.use(morgan('common'));
+
+// GET requests
+app.get('/movies', (req, res) => {
+        res.json(topMovies);
+    }
+
+);
+
+app.get('/', (req, res) => {
+        res.send('Placeholder text for a future app!');
+    }
+
+);
+
+// Serve static files
+app.use(express.static('public'));
+
+// Error handling
+app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    }
+
+);
+
+// Listen for requests
+app.listen(8080, () => {
+        console.log('Your app is listening on port 8080.');
+    }
+
+);
