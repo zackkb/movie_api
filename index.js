b6ecca83f9f6a14a51df77d8c252abd6d05a8cfd
zackkb <98679473+zackkb@users.noreply.github.com>
2022-03-25 23:12:16 -0500

    Added an array to define topMovies
    Added an array of top 10 movies to define object JSON

--- index.js
+++ index.js
@@ -7,16 +7,79 @@ const app = express();
 // Morgan middleware library to log all requests
 app.use(morgan('common'));
 
+// An array of top 10 movies
+let topMovies = [{
+        title: 'Letter to Momo',
+        director: 'Hiroyuki Okiura',
+        genre: 'animated, drama',
+        released: '2011',
+    },
+    {
+        title: 'The Boy And The Beast',
+        director: 'Mamoru Hosoda',
+        genre: 'animated, action-adventure, fantasy',
+        released: '2015',
+    },
+    {
+        title: 'A Silent Voice',
+        director: 'Naoko Yamada',
+        genre: 'animated, drama',
+        released: '2016',
+    },
+    {
+        title: 'From Up On Poppy Hill',
+        director: 'Gorō Miyazaki',
+        genre: 'animated, drama',
+        released: '2011',
+    },
+    {
+        title: 'Tokyo Godfathers',
+        director: 'Satoshi Kon',
+        genre: 'animated, tragicomedy, adventure',
+        released: '2003',
+    },
+    {
+        title: 'Miss Hokusai',
+        director: 'Keiichi Hara',
+        genre: 'animated, historical',
+        released: '2015',
+    },
+    {
+        title: 'The Place Promised In Our Early Days',
+        director: 'Makoto Shinkai',
+        genre: 'animated, alternate-history',
+        released: '2004',
+    },
+    {
+        title: 'Night Is Short, Walk On Girl',
+        director: 'Masaaki Yuasa',
+        genre: 'animated, romantic-comedy',
+        released: '2017',
+    },
+    {
+        title: 'Mirai',
+        director: 'Mamoru Hosoda',
+        genre: 'animated, adventure, fantasy, comedy',
+        released: '2018',
+    },
+    {
+        title: 'Millennium Actress',
+        director: 'Satoshi Kon',
+        genre: 'animated, drama',
+        released: '2001',
+    },
+];
+
 // GET requests
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
 app.get('/', (req, res) => {
-    res.send('Placeholder text for a future app!');
+    res.send('Welcome to my app!');
 });
 
-// Serve static files
+// Serve static content from 'public' directory
 app.use(express.static('public'));
 
 // Error handling
