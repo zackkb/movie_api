de855df734409fc07fd352d8c1a490c1de2f92d5
zackkb <98679473+zackkb@users.noreply.github.com>
2022-05-17 00:25:31 -0500

    re-adding authentication

--- index.js
+++ index.js
@@ -57,16 +57,18 @@ app.use(morgan("common"));
 
 // GET requests
 
-app.get("/movies", function (req, res) {
+app.get('/movies', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
     Movies.find()
-      .then(function (movies) {
-        res.status(201).json(movies);
-      })
-      .catch(function (error) {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      });
-  });
+        .then((movie) => {
+            res.status(201).json(movie);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 // READ Return data about a single movie by title
 app.get('/movies/:Title', passport.authenticate('jwt', {
